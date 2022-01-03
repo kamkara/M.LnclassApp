655e3e967ef90e01febff0554411ec8ef95caafa
kamkara <lnclass.kamara+ln@gmail.com>
2022-01-03 19:20:18 +0000

    add classroom controller

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,5 +1,9 @@
 class ClassroomsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_classroom, only: %i[ show edit update destroy ]
+  before_action :find_levels
+  before_action :find_schools
+
 
   # GET /classrooms or /classrooms.json
   def index
@@ -8,8 +12,10 @@ class ClassroomsController < ApplicationController
 
   # GET /classrooms/1 or /classrooms/1.json
   def show
+    if current_user.role == "Student"
+      render :teacher_view_classroom
+    end
   end
-
   # GET /classrooms/new
   def new
     @classroom = Classroom.new
@@ -21,11 +27,11 @@ class ClassroomsController < ApplicationController
 
   # POST /classrooms or /classrooms.json
   def create
-    @classroom = Classroom.new(classroom_params)
+    @classroom = current_user.classrooms.build(classroom_params)
 
     respond_to do |format|
       if @classroom.save
-        format.html { redirect_to classroom_url(@classroom), notice: "Classroom was successfully created." }
+        format.html { redirect_to classrooms_path, notice: "Classroom was successfully created." }
         format.json { render :show, status: :created, location: @classroom }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +44,7 @@ class ClassroomsController < ApplicationController
   def update
     respond_to do |format|
       if @classroom.update(classroom_params)
-        format.html { redirect_to classroom_url(@classroom), notice: "Classroom was successfully updated." }
+        format.html { redirect_to @classroom, notice: "Classroom was successfully updated." }
         format.json { render :show, status: :ok, location: @classroom }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -50,7 +56,6 @@ class ClassroomsController < ApplicationController
   # DELETE /classrooms/1 or /classrooms/1.json
   def destroy
     @classroom.destroy
-
     respond_to do |format|
       format.html { redirect_to classrooms_url, notice: "Classroom was successfully destroyed." }
       format.json { head :no_content }
@@ -60,11 +65,19 @@ class ClassroomsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_classroom
-      @classroom = Classroom.find(params[:id])
+      @classroom = Classroom.friendly.find(params[:id])
+    end
+
+    def find_levels
+      @levels = Level.all
+    end
+
+    def find_schools
+      @schools = School.all
     end
 
     # Only allow a list of trusted parameters through.
     def classroom_params
-      params.require(:classroom).permit(:name, :slug, :user_id, :level_id, :material_id, :school_id)
+      params.require(:classroom).permit(:name, :slug, :level_id, :school_id, :material_id, :user_id)
     end
 end
--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,6 +1,35 @@
 class Classroom < ApplicationRecord
-  belongs_to :user
+  ############## RELATIONS  #############
   belongs_to :level
   belongs_to :material
   belongs_to :school
+  belongs_to :user
+  has_many :exercices,
+            dependent: :destroy
+  has_one :classroom_admin,
+          dependent: :delete
+
+############## VALIDADTIONS  #############
+
+validates_with ClassroomValidator
+validates :name, 
+          :slug, 
+          :user_id, 
+          :school_id,
+          :material_id,
+          :level_id,  presence: true
+
+
+############## SLUG #############
+  extend FriendlyId
+  friendly_id :slugged_custom, use: :slugged
+
+  def should_generate_new_friendly_id?
+    name_changed? || super
+  end
+
+  def slugged_custom
+    #{school_name.nick_name} #
+    "from #{name}"
+  end
 end
