7ddd19bc5b2b7c782bef228e559b5ab07b904790
kamkara <lnclass.kamkara@gmail.com>
2022-01-20 15:56:06 +0000

    add user sign in dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,12 @@
 class DashboardController < ApplicationController
+  before_action :cityEreas
+
   def index
+    @UserDashboard = User.where("role = ?", "Student").order('created_at desc')
   end
+
+  private
+    def cityEreas
+      @city_ereas = CityErea.all
+    end
 end
