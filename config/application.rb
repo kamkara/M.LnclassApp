c8a2e31562c2ac93f12455ce3f50dec1a1e4acf9
kamkara <lnclass.kamkara@gmail.com>
2022-01-20 12:38:09 +0000

    add dynamic errors page

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module Lnclassapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    
+    #Add errors routes
+    config.exceptions_app = self.routes
 
     # Configuration for the application, engines, and railties goes here.
     #
