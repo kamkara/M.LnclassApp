dd6799cbd928e8ef6b4c4acbedbbaae5383354dd
kamkara <lnclass.kamkara@gmail.com>
2022-01-23 16:25:13 +0000

    add User column

--- db/migrate/20220123133118_add_material_and_level_to_users.rb
+++ db/migrate/20220123133118_add_material_and_level_to_users.rb
@@ -1,8 +1,8 @@
 class AddMaterialAndLevelToUsers < ActiveRecord::Migration[6.1]
   def change
+    add_column :users, :material_name, :string
     add_column :users, :level_name, :string
     add_column :users, :school_name, :string
     add_column :users, :city_name, :string
-    add_column :users, :material_name, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_22_162933) do
+ActiveRecord::Schema.define(version: 2022_01_23_133118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -236,6 +236,10 @@ ActiveRecord::Schema.define(version: 2022_01_22_162933) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.uuid "city_id"
+    t.string "material_name"
+    t.string "level_name"
+    t.string "school_name"
+    t.string "city_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
