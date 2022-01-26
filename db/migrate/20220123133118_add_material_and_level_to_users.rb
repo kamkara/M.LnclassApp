3d8f91a7b96a34eec980cd0573b31ae14ba3a8d6
kamkara <lnclass.kamkara@gmail.com>
2022-01-26 19:36:53 +0000

    comment Mateial_name

--- db/migrate/20220123133118_add_material_and_level_to_users.rb
+++ db/migrate/20220123133118_add_material_and_level_to_users.rb
@@ -1,6 +1,6 @@
 class AddMaterialAndLevelToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :material_name, :string
+    #add_column :users, :material_name, :string
     add_column :users, :level_name, :string
     add_column :users, :school_name, :string
     add_column :users, :city_name, :string
